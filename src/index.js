6edef5f72ba7defcf5449e6a78c9ac87cdf86e22
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-27 14:43:27 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 //const mult = require("./calc")
 import {add,mult} from "./calc"
+import("./index.css");
 
-import("./index.css")
+import React from "react";
+import ReactDOM from "react-dom";
+
 console.log(mult(7,10))
 
-const h1 = document.createElement("h1");
-h1.innerHTML = "Hello Webpack";
-h1.classList.add("redtext");
+// const h1 = document.createElement("h1");
+// h1.innerHTML = "Hello Webpack";
+// h1.classList.add("redtext");
+// document.getElementById("root").appendChild(h1);
 
-document.getElementById("root").appendChild(h1);
+ReactDOM.render(
+    React.cloneElement("h1",{className:"greencolor"},"Hello React") //(element,object(or key-value pair),another object or plane text)
+)
